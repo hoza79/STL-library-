09876ef144389ccf4f2d7b5746fc09921e36dd5b
Hosam <hoza2100@student.miun.se>
2025-01-17 12:43:58 +0100

    Initial commit

---
+++ STL-library/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  STL-library
+//
+//  Created by hosan on 2025-01-17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
